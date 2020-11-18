cd53b29d16bb9a73ca86392c50cc1a6bbcb64d37
Brian Abeyta-Pratt <babeytapratt@gmail.com>
2020-11-18 13:08:18 -0700

    MyItemsList is all items with non-working buttons

--- src/components/MyItemsList.js
+++ src/components/MyItemsList.js
@@ -1,43 +1,71 @@
 import react, { useState, useEffect, useContext } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import axios from 'axios';
+import axiosWithAUth from './../utils/axiosWithAuth';
 import { MarketContext } from '../contexts/MarketContext';
-
-const myInitialItems = {
-    name: '',
-    category: '',
-    price: '',
-    location: ''
-};
+import Item from '../components/Item'
 
 const MyItemsList = () => {
+    const [items, setItems, locations, categories] = useContext(MarketContext);
+    const [categoryValue, setCategoryValue] = useState("All Categories");
+    const [locationValue, setLocationValue] = useState("All Locations");
 
-    const [items, setItems, locations, categories, currentUser] = useContext(MarketContext)
-    const [myItems, setMyItems] = useState(myInitialItems)
+    const [currentItems, setCurrentItems] = useState(items);
+    const [locationsList, setLocationsList] = useState([]);
 
 
+  useEffect(() => {
+    getItemsData();
+  }, []);
 
-    useEffect(() => {
-        console.log(items);
-        const getMyItems = () => {
-            axios
-                .get('')
-                .then(res => {
-                    getMyItems(res.data);
-                })
-                .catch(error => console.log("You have an error", error));
-        };
-    }, []);
+  const getItemsData = () => {
+    axiosWithAUth()
+      .get("/items")
+      .then((req) => {
+        console.log(req);
+        setCurrentItems(req.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  let categoryOptions = categories.map((category) => (
+    <option key={category}>{category}</option>
+  ));
+
+  categoryOptions.unshift("All Categories");
+
+  let locationOptions = locations.map((location) => (
+    <option key={location}>{location}</option>
+  ));
+
+  locationOptions.unshift("All Locations");
 
     return (
-        <div className='myItemsList'>
-            <h1>My store</h1>
-            <p>Name: {myItems.name}</p>
-            <p>Category:{myItems.category}</p>
-            <p>Price:{myItems.price}</p>
-            <p>Location:{myItems.location}</p>
+        <div className='ItemsList'>
+                <h1>My Marketplace</h1>
+                <div className='items'>
+                    {currentItems.map((item) => (
+                    <Item itemData={item} key={item.id} />
+                    ))}
+                </div>
+                <button className='my-marketplace-button'>Add Item</button>
+                <button className='my-marketplace-button'>Delete Item</button>
         </div>
-    )
-}
+    );
+};
 
-export default MyItemsList;
+export default MyItemsList
+
+// {/* export default MyItemsList;
+//         // <div className='myItemsList'>
+//         //     <h1>My store</h1>
+//         //     <p>Name: {myItems.name}</p>
+//         //     <p>Category:{myItems.category}</p>
+//         //     <p>Price:{myItems.price}</p>
+//         //     <p>Location:{myItems.location}</p>
+//         // </div>
+// //     )
+// // }
+
+// // export default MyItemsList; */}
