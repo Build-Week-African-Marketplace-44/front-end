3de0d209fd0b682faeb49999028983f4e8b48dbc
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-11-18 10:42:46 -0500

    marketplace and items styles

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,16 +1,19 @@
 import React from "react";
 
+import './Item.css'; //styles
 
 
 const Item = (props) => {
   return (
-    <div>
+    <div className="market-item">
       <h3>{props.itemData.name}</h3>
-      <p>Price: {props.itemData.price}</p>
-      {/* <p>Seller: {props.itemData.seller}</p> */}
-      <p>Market Location: {props.itemData.location}</p>
-      <p>Category: {props.itemData.category}</p>
-      <p></p>
+      <div className="item-bottom">
+        <p>Price: {props.itemData.price}</p>
+        {/* <p>Seller: {props.itemData.seller}</p> */}
+        <p>Market Location: {props.itemData.location}</p>
+        <p>Category: {props.itemData.category}</p>
+        <p></p>
+      </div>
     </div>
   );
 };
