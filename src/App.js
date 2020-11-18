54a6cb8a512010cd6fb2f9fc4d3db0ee0b5a95f1
Matthew Serwer <Matthew.Serwer@gmail.com>
2020-11-17 19:32:21 -0500

    fixed merged remnant

--- src/App.js
+++ src/App.js
@@ -6,14 +6,6 @@ import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import { productList, locationsData, categoriesData } from "./data";
 
 //components
-<<<<<<< HEAD
-import Dashboard from './components/Dashboard';
-import MyItemsList from './components/MyItemsList';
-import ProfileForm from './forms/ProfileForm';
-import NewItemForm from './forms/NewItemForm';
-import ItemsList from './components/ItemsList';
-import Landing from './components/Landing';
-=======
 import Dashboard from "./components/Dashboard";
 import MyItemsList from "./components/MyItemsList";
 import ProfileForm from "./forms/ProfileForm";
@@ -24,7 +16,6 @@ import Login from "./forms/Login";
 
 import PrivateRoute from "./components/PrivateRoute";
 
->>>>>>> a2583edc03bf206bb1794b465b64b25b73a4e301
 // contexts
 import { MarketContext } from "./contexts/MarketContext";
 
@@ -41,21 +32,6 @@ const App = () => {
 
   return (
     <MarketContext.Provider value={[items, setItems, locations, categories]}>
-<<<<<<< HEAD
-      
-        <div className="App">
-          {/* <header className="App-header">
-            <h1>African Marketplace</h1>
-          </header> */}
-            <Dashboard />
-            <Router>
-            <Switch>
-              <Route path='/Landing' component={Landing}/> 
-              <Route exact path='/' component={MyItemsList} />
-              <Route path='/marketplace' component={ItemsList} />
-              <Route path='/new-item' component={NewItemForm} />
-              <Route path='/profile' component={ProfileForm} />
-=======
       <div className='App'>
         <header className='App-header'>
           <h1>African Marketplace</h1>
@@ -75,7 +51,6 @@ const App = () => {
                 return <Login {...props} setIsLoggedIn={setIsLoggedIn} />;
               }}
             />
->>>>>>> a2583edc03bf206bb1794b465b64b25b73a4e301
           </Switch>
         </Router>
       </div>
