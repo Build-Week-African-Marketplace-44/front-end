cdf8ff4431075ca1c964829864cea9141770a75d
Matthew Serwer <Matthew.Serwer@gmail.com>
2020-11-19 18:15:48 -0500

    tweeked router to not rerender landing

--- src/App.js
+++ src/App.js
@@ -14,19 +14,17 @@ import ItemsList from "./components/ItemsList";
 import ItemPage from "./components/ItemPage";
 import Signup from "./forms/Signup";
 import Login from "./forms/Login";
-import Landing from './components/Landing'
-import Footer from './components/Footer'
+import Landing from "./components/Landing";
+import Footer from "./components/Footer";
 
 import PrivateRoute from "./components/PrivateRoute";
-import axiosWithAuth from "./utils/axiosWithAuth"
-
+import axiosWithAuth from "./utils/axiosWithAuth";
 
 // contexts
 import { MarketContext } from "./contexts/MarketContext";
 
 // styles
-import './style/App.css'
-
+import "./style/App.css";
 
 // export const MarketContext = createContext();
 const App = () => {
@@ -35,68 +33,81 @@ const App = () => {
   const [categories, setCategories] = useState(categoriesData);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [myUserId, setMyUserId] = useState("");
-  const [myUserData, setMyUserData] = useState([])
+  const [myUserData, setMyUserData] = useState([]);
   const [userList, setUserList] = useState([]);
 
-useEffect(() => {
-  if(localStorage.getItem('token')) {
-    setIsLoggedIn(true);
-  }
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setIsLoggedIn(true);
+    }
 
-  if(localStorage.getItem('myUserId')) {
-    setMyUserId(localStorage.getItem('myUserId'))
-    console.log(`useridhere: ${myUserId}`);
-  }
-  getItemsData()
-  console.log(`user Data: ${myUserData}`);
-  // setMyUserId(localStorage.getItem('myUserId'));
-  // console.log(myUserId);
-  // getItemsData();
-}, [])
+    if (localStorage.getItem("myUserId")) {
+      setMyUserId(localStorage.getItem("myUserId"));
+      console.log(`useridhere: ${myUserId}`);
+    }
+    getItemsData();
+    console.log(`user Data: ${myUserData}`);
+    // setMyUserId(localStorage.getItem('myUserId'));
+    // console.log(myUserId);
+    // getItemsData();
+  }, []);
 
-const getItemsData = () => {
-  axiosWithAuth()
-    .get("/items")
-    .then((req) => {
-      setMyUserData(
-        req.data.filter((item) => {
-          // console.log(`item:${item.user_id} Mine:${myUserId}`)
-          console.log(item)
-          return item.user_id === myUserId;
-        })
-      );
-      // setMyItems(req.data)
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
-
+  const getItemsData = () => {
+    axiosWithAuth()
+      .get("/items")
+      .then((req) => {
+        setMyUserData(
+          req.data.filter((item) => {
+            // console.log(`item:${item.user_id} Mine:${myUserId}`)
+            console.log(item);
+            return item.user_id === myUserId;
+          })
+        );
+        // setMyItems(req.data)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
-    <MarketContext.Provider value={[items, setItems, locations, categories, myUserId, setMyUserId, userList, setUserList]}>
+    <MarketContext.Provider
+      value={[
+        items,
+        setItems,
+        locations,
+        categories,
+        myUserId,
+        setMyUserId,
+        userList,
+        setUserList,
+      ]}
+    >
       <div className='App'>
-
         <Router>
-          {!isLoggedIn ? <Landing setIsLoggedIn={setIsLoggedIn} /> : isLoggedIn}
           {isLoggedIn ? <Dashboard setIsLoggedIn={setIsLoggedIn} /> : null}
+
           <Switch>
             <PrivateRoute exact path='/mystore' component={MyItemsList} />
             <PrivateRoute exact path='/marketplace' component={ItemsList} />
             <PrivateRoute exact path='/new-item' component={NewItemForm} />
-            <PrivateRoute path="/item/:id" component={ItemPage} />
+            <PrivateRoute path='/item/:id' component={ItemPage} />
             <PrivateRoute exact path='/profile' component={ProfileForm} />
-            <Route exact path="/" component={Landing} />
+            <Route exact path='/' component={Landing} />
             <Route path='/signup' component={Signup} />
             <Route
               path='/login'
               render={(props) => {
-                return <Login {...props} setIsLoggedIn={setIsLoggedIn} setMyUserId={setMyUserId}/>;
+                return (
+                  <Login
+                    {...props}
+                    setIsLoggedIn={setIsLoggedIn}
+                    setMyUserId={setMyUserId}
+                  />
+                );
               }}
             />
-            <Route exact path='/landing' component={Landing} />
           </Switch>
-          <Footer/>
         </Router>
       </div>
     </MarketContext.Provider>
